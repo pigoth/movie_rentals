e51bd91214a32418c05181ed88bff5a783359404
TFerrari <tferrari@xanadu.ie>
2025-05-13 20:00:08 +0200

    customer: extract method frequent renter points from statement

--- customer.js
+++ customer.js
@@ -14,26 +14,31 @@ export class Customer {
 
     statement() {
         let totalAmount = 0
-        let frequentRenterPoints = 0
+        let totalFrequentRenterPoints = 0
         let result = "Rental record for " + this.name + "\n"
         // determine amounts for each line
         for (let i = 0; i < this.rentals.length; i++) {
             let each = this.rentals[i]
             let thisAmount = each.fee(each);
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two-day new-release rental
-            if ((each.movie.priceCode.name === 'NEW RELEASE') && (each.daysRented > 1)) {
-                frequentRenterPoints++;
-            }
+            let frequentRenterPoints = this.frequentRenterPoints(each);
             // show figures for this rental
             result += "\t" + each.movie.title + "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
+            totalFrequentRenterPoints += frequentRenterPoints;
         }
         // add footer lines
         result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points.";
+        result += "You earned " + totalFrequentRenterPoints + " frequent renter points.";
         return result;
     }
 
+    frequentRenterPoints(each) {
+        let frequentRenterPoints = 1;
+        // add frequent renter points
+        // add bonus for a two-day new-release rental
+        if ((each.movie.priceCode.name === 'NEW RELEASE') && (each.daysRented > 1)) {
+            frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
 }

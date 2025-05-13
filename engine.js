78c0d3b2f8d3c91906eb696240a376c7cdd6a429
TFerrari <tferrari@xanadu.ie>
2025-05-13 19:19:55 +0200

    move Custome on a specific file

--- engine.js
+++ engine.js
@@ -1,3 +1,5 @@
+import {Customer} from "./customer";
+
 class PriceCode {
   constructor(name) {
     this.name = name
@@ -11,60 +13,10 @@ class Movie {
   }
 }
 
-class Rental {
+export class Rental {
   constructor(movie, daysRented) {
     this.movie = movie
     this.daysRented = daysRented
-  }
-}
-
-class Customer {
-  constructor(name) {
-    this.name = name
-    this.rentals = []
-  }
-  addRental(movie, daysRented) {
-    let rental = new Rental(movie, daysRented);
-    this.rentals.push(rental)
-    return rental;
-  }
-  statement() {
-    let totalAmount = 0
-    let frequentRenterPoints = 0
-    let result = "Rental record for " + this.name + "\n"
-    // determine amounts for each line
-    for (let i=0; i<this.rentals.length; i++) {
-      let each = this.rentals[i]
-      let thisAmount = 0
-      if (each.movie.priceCode.name === 'REGULAR') {
-        thisAmount += 2
-        if (each.daysRented > 2) {
-          thisAmount += ((each.daysRented - 2) * 1.5);          
-        }
-      }
-      else if (each.movie.priceCode.name === 'NEW RELEASE') {
-        thisAmount += each.daysRented * 3
-      }
-      else if (each.movie.priceCode.name === 'CHILDRENS') {
-        thisAmount += 1.5;
-        if (each.daysRented > 3) {
-          thisAmount = (each.daysRented - 3) * 1.5;
-        }
-      }
-      // add frequent renter points
-      frequentRenterPoints++;
-      // add bonus for a two-day new-release rental
-      if ((each.movie.priceCode.name === 'NEW RELEASE') && (each.daysRented > 1)) {
-        frequentRenterPoints ++;
-      }
-      // show figures for this rental
-      result += "\t" + each.movie.title + "\t" + thisAmount + "\n";
-      totalAmount += thisAmount;
-    }
-    // add footer lines
-    result += "Amount owed is " + totalAmount + "\n";
-    result += "You earned " + frequentRenterPoints + " frequent renter points.";
-    return result;
   }
 }
 

44afb1ba91a7380b5d6b02cb9eda30bd29132424
TFerrari <tferrari@xanadu.ie>
2025-05-13 20:27:11 +0200

    customer: logically split concepts in statement

--- customer.js
+++ customer.js
@@ -11,16 +11,23 @@ export class Customer {
 
     statement() {
         let totalAmount = 0
-        let totalFrequentRenterPoints = 0
-        let result = "Rental record for " + this.name + "\n"
+        for (let i = 0; i < this.rentals.length; i++) {
+            let rental = this.rentals[i]
+            totalAmount += rental.fee();
+        }
 
+        let totalFrequentRenterPoints = 0
+        for (let i = 0; i < this.rentals.length; i++) {
+            let rental = this.rentals[i]
+            totalFrequentRenterPoints += rental.frequentRenterPoints();
+        }
+
+        let result = "Rental record for " + this.name + "\n"
         // determine amounts for each line
         for (let i = 0; i < this.rentals.length; i++) {
             let rental = this.rentals[i]
             // show figures for this rental
             result += "\t" + rental.movie.title + "\t" + rental.fee() + "\n";
-            totalAmount += rental.fee();
-            totalFrequentRenterPoints += rental.frequentRenterPoints();
         }
         // add footer lines
         result += "Amount owed is " + totalAmount + "\n";

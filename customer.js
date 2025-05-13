3a583f606d72747b3bcfbf5928cdf5cbd8f2ddbe
TFerrari <tferrari@xanadu.ie>
2025-05-13 20:24:36 +0200

    customer: inline vars in statement

--- customer.js
+++ customer.js
@@ -13,15 +13,14 @@ export class Customer {
         let totalAmount = 0
         let totalFrequentRenterPoints = 0
         let result = "Rental record for " + this.name + "\n"
+
         // determine amounts for each line
         for (let i = 0; i < this.rentals.length; i++) {
             let rental = this.rentals[i]
-            let thisAmount = rental.fee();
-            let frequentRenterPoints = rental.frequentRenterPoints();
             // show figures for this rental
-            result += "\t" + rental.movie.title + "\t" + thisAmount + "\n";
-            totalAmount += thisAmount;
-            totalFrequentRenterPoints += frequentRenterPoints;
+            result += "\t" + rental.movie.title + "\t" + rental.fee() + "\n";
+            totalAmount += rental.fee();
+            totalFrequentRenterPoints += rental.frequentRenterPoints();
         }
         // add footer lines
         result += "Amount owed is " + totalAmount + "\n";
